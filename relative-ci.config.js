fcb069b939160721d692de1eba562aebd6d4a8fd
Viorel Cojocaru <vio@beanon.com>
2025-11-05 23:29:47 +0100

    build: RelativeCI - ingest compression

--- relative-ci.config.js
+++ relative-ci.config.js
@@ -4,6 +4,7 @@
 module.exports = {
   payloadFilepath: './artifacts/relative-ci-agent.json',
   includeCommitMessage: true,
+  compress: true,
   webpack: {
     // Path to Webpack stats JSON file
     stats: './artifacts/webpack-stats.json',
